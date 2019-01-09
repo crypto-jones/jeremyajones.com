bd7668b865e1d9f660f5414bb802b2d984c85e36
crypto-jones <integral.being@gmail.com>
2019-01-08 18:02:08 -0800

    Complete homepage component with particle effects

---
+++ client/src/components/Home/HomeStyles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  background: #252934;
+  height: 100vh;
+`;
+
+export const HeaderContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Arvo:400,700');
+  position: relative;
+  top: 40%;
+  text-align: center;
+  font-family: 'Arvo', serif;
+  font-size: 2.2rem;
+  color: white;
+`;
+
+export const Highlight = styled.div`
+  font-size: 3rem;
+  padding-bottom: 5px;
+  font-weight: 700;
+  color: #e31b6d;
+`;
