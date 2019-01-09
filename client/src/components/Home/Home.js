c5bd0c822399675fa9d45d5cebbb311d931deebd
crypto-jones <integral.being@gmail.com>
2019-01-08 19:11:26 -0800

    Separate Particles into own component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,124 +1,18 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
 import {
   HomeContainer,
   HeaderContainer,
   TextContainer,
   Highlight,
-} from './HomeStyles.js';
-import NavButton from '../Button/Button.js';
+} from './HomeStyles';
+import NavButton from '../Button/Button';
+import Particles from '../Particles/Particles';
 
 class Home extends Component {
   render() {
     return (
       <HomeContainer>
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: 80,
-                density: {
-                  enable: true,
-                  value_area: 800,
-                },
-              },
-              color: {
-                value: ['#E31B6D', '#00d8ff'],
-              },
-              shape: {
-                type: 'circle',
-                stroke: {
-                  width: 0,
-                  color: '#000000',
-                },
-                polygon: {
-                  nb_sides: 5,
-                },
-              },
-              opacity: {
-                value: 1,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 40,
-                  size_min: 0.1,
-                  sync: false,
-                },
-              },
-              line_linked: {
-                enable: true,
-                distance: 225,
-                color: '#fff',
-                opacity: 0.9,
-                width: 0.2,
-              },
-              move: {
-                enable: true,
-                speed: 3,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200,
-                },
-              },
-            },
-            interactivity: {
-              detect_on: 'canvas',
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'repulse',
-                },
-                onclick: {
-                  enable: true,
-                  mode: 'push',
-                },
-                resize: true,
-              },
-              modes: {
-                grab: {
-                  distance: 400,
-                  line_linked: {
-                    opacity: 1,
-                  },
-                },
-                bubble: {
-                  distance: 400,
-                  size: 40,
-                  duration: 2,
-                  opacity: 1,
-                  speed: 3,
-                },
-                repulse: {
-                  distance: 100,
-                  duration: 0.4,
-                },
-                push: {
-                  particles_nb: 4,
-                },
-                remove: {
-                  particles_nb: 2,
-                },
-              },
-            },
-            retina_detect: true,
-          }}
-        />
+        <Particles />
         <HeaderContainer>
           <TextContainer>
             <Highlight>Jeremy A. Jones</Highlight>
